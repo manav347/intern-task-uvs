ffc102c8e2e1d569ef9ede07fe8748dbf208df21
Manav Patel <manavp347@gmail.com>
2021-12-02 11:58:36 +0530

    Ameya changes

--- src/components/Cart/cart.js
+++ src/components/Cart/cart.js
@@ -1,14 +1,14 @@
 import React from "react";
 import "../Cart/cart.css";
 
-const Cart = ({cartItems, handleAddProduct, handleRemoveProduct, handleCartClearance})=>{
+const Cart = ({ cartItems, handleAddProduct, handleRemoveProduct, handleCartClearance }) => {
     console.log(cartItems)
 
     const totalPrice = cartItems.reduce(
-        (price, item)=> price+ item.quantity * item.price, 0
+        (price, item) => price + item.quantity * item.price, 0
     );
 
-    return(
+    return (
         <div className="cart-items-con">
             <h3 className="cart-header flex-start">Cart Items</h3>
             <div className="clear-cart flex-end">
@@ -16,32 +16,83 @@ const Cart = ({cartItems, handleAddProduct, handleRemoveProduct, handleCartClear
             </div>
             {cartItems.length === 0 && <div className="no-item">no items</div>}
 
-            {cartItems.map((item)=>(
-                <div key={item.id} className="cart-item">
-                    <img className="cart-item-image" src={item.image} alt={item.name}></img>
-                    <div className="cart-item-name"><h4>{item.name}</h4></div>
-                    <div className="cart-function">
-                        <button onClick={()=>handleAddProduct(item)} className="add">
-                        <i className="fas fa-plus"></i>
-                        </button>
-                        <button onClick={()=>handleRemoveProduct(item)}  className="sub">
-                        <i className="fas fa-minus"></i>
-                        </button>
-                    </div>
-                    {/* <div className="card-item-quantity">{item.quantity}</div> */}
-                    <div className="card-item-price">{item.quantity} * ${item.price}</div>
-                </div>
-            ))}
+            <div id="Main-page row">
+                <div className="row main-con">
+                    {cartItems.map((item) => (
 
+
+                        <div key={item.id} className="">
+
+                            {/*                 TEST
+                    <div>
+                        <img src={productItem.image} alt={productItem.name}></img>
+                        <h1 className="produ" >{productItem.name}</h1>
+                    </div> */}
+
+
+                            <div>
+                                <div className="pro-card ">
+                                    <div className="pro-card-top">
+                                        <h3 className="pro-card-heading">
+
+                                            <img className="shoeimage" src={item.image} alt={item.name}></img>
+                                            {/* <img className="shoeimage" src={productItem.image} alt={productItem.key}></img> */}
+                                        </h3>
+                                    </div>
+                                    <div className="pro-card-bottom">
+                                        <div className="pro-card-head-con">
+
+                                            <div className="cart-item-name"><h4>{item.name}</h4></div>
+                                            {/* <p className="pro-card-head">{productItem.name}</p> */}
+
+                                            <div className="cart-function">
+                                                <button onClick={() => handleAddProduct(item)} className="add">
+                                                    <i className="fas fa-plus"></i>
+                                                </button>
+                                                <button onClick={() => handleRemoveProduct(item)} className="sub">
+                                                    <i className="fas fa-minus"></i>
+                                                </button>
+                                            </div>
+                                        </div>
+
+                                        <div className="pro-card-link-con row">
+                                            <div style={{ padding: "0 15px 0" }} className="card-item-price">{item.quantity} * ${item.price}</div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+
+                        </div>
+                    ))}
+                </div>
+            </div>
             <div className="total-price-con">
                 <h3 className="total-price">
                     ${totalPrice}
                 </h3>
             </div>
 
-                
+
         </div>
     );
 }
 
 export default Cart;
+
+
+
+// <div key={item.id} className="cart-item">
+//     <img className="cart-item-image" src={item.image} alt={item.name}></img>
+//     <div className="cart-item-name"><h4>{item.name}</h4></div>
+//     <div className="cart-function">
+//         <button onClick={() => handleAddProduct(item)} className="add">
+//             <i className="fas fa-plus"></i>
+//         </button>
+//         <button onClick={() => handleRemoveProduct(item)} className="sub">
+//             <i className="fas fa-minus"></i>
+//         </button>
+//     </div>
+//     {/* <div className="card-item-quantity">{item.quantity}</div> */}
+//     <div className="card-item-price">{item.quantity} * ${item.price}</div>
+// </div>
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,19 +1,19 @@
 import React from 'react';
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 import "../components/Nav.css"
 
-const Nav=()=>{
-    return(
+const Nav = () => {
+    return (
 
         <div id="navid">
-            <nav className="navbar fixed-top navbar-expand-lg"> 
-                <Link className="navbar-brand" to="/">MHood</Link>
+            <nav className="navbar fixed-top navbar-expand-lg">
+                <Link className="navbar-brand" to="/">AHood</Link>
                 <Link className="nav-link-cart" to="/Cart"><i className="fas fa-shopping-cart"></i></Link>
             </nav>
         </div>
-        
+
     );
-    
+
 }
 
 export default Nav;
